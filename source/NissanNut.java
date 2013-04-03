5e62f288e0ef224c8c8eefec2931ae0a4ec79390
xor <nztrooy@gmail.com>
2013-04-03 21:19:22 +1300

    Advice

---
+++ source/NissanNut.java
@@ -0,0 +1,9 @@
+/**
+ * @Author xor
+ * 8:24 PM 4/3/13
+ */
+public class NissanNut {
+    public static void main(String[] args){
+        System.out.println("Your commits are bad, \nand you should feel bad.");
+    }
+}
