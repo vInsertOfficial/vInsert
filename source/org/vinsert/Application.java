0b23386ef2504d4844c548b05ff1559b39742f94
iamtommo <iamtommo@gmail.com>
2013-04-03 21:06:34 +0200

    Stack trace output dialog

--- source/org/vinsert/Application.java
+++ source/org/vinsert/Application.java
@@ -1,11 +1,14 @@
 package org.vinsert;
 
+import java.lang.Thread.UncaughtExceptionHandler;
+
 import javax.swing.JFrame;
 import javax.swing.JPopupMenu;
 import javax.swing.SwingUtilities;
 import javax.swing.UIManager;
 
 import org.pushingpixels.substance.api.skin.SubstanceGraphiteGlassLookAndFeel;
+import org.vinsert.bot.ui.BotErrorDialog;
 import org.vinsert.bot.ui.BotVersionChecker;
 import org.vinsert.bot.ui.BotWindow;
 import org.vinsert.bot.util.VSecruityManager;
@@ -26,6 +29,18 @@ public class Application {
 					JFrame.setDefaultLookAndFeelDecorated(true);
 					JPopupMenu.setDefaultLightWeightPopupEnabled(false);
 			       		System.setSecurityManager(new VSecruityManager());
+			       		
+			       	Thread.setDefaultUncaughtExceptionHandler(new UncaughtExceptionHandler() {
+						@Override
+						public void uncaughtException(final Thread t, final Throwable e) {
+							SwingUtilities.invokeLater(new Runnable() {
+								@Override
+								public void run() {
+									BotErrorDialog.show("Error in thread: " + t.getName(), e);
+								}
+							});
+						}
+			       	});
 					
 					if(Configuration.OFFLINE) {
 						window = new BotWindow();
