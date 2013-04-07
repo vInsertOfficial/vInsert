a105b15dcb1b2c4d94e43df0b6f78ff81f2581bb
DarkStorm <darkstorm@evilminecraft.net>
2013-04-07 15:09:24 -0400

    Revert "Fix BotScriptViewer modality... JDialogs freeze the thread when modal."
    This reverts commit 1cd6cc47ce993948e0302c9018d9f4176c9872fa.

--- source/org/vinsert/bot/ui/BotScriptViewer.java
+++ source/org/vinsert/bot/ui/BotScriptViewer.java
@@ -57,6 +57,9 @@ public class BotScriptViewer extends JDialog {
 	public BotScriptViewer() {
 		super(Application.window.getFrame());
 		
+		setModalityType(ModalityType.APPLICATION_MODAL);
+		setModal(true);
+		
 		model = new DefaultTableModel(new Object[] { "Script", "Version", "Description", "Authors" }, 0);
 		table = new JTable();
 		table.setModel(model);
