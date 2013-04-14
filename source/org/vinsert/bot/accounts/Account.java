a4108f725ad8523125105e2950e71a50bc153d57
results <sejteit@gmail.com>
2013-04-14 16:15:59 -0300

    Added base64 encryption to stored accounts.

--- source/org/vinsert/bot/accounts/Account.java
+++ source/org/vinsert/bot/accounts/Account.java
@@ -1,44 +1,47 @@
 package org.vinsert.bot.accounts;
 
+import org.vinsert.bot.util.Cryption;
+
 /**
  * @author iJava
  */
 public class Account {
 
-    private final String username;
-    private final String password;
-    private final String pin;
-    private final String reward;
+    private static String username;
+	private static String password;
+	private static String pin;
+	private static String reward;
 
-    public Account(final String username, final String password, final String pin, final String reward) {
-        this.username = username;
-        this.password = password;
-        this.pin = pin;
-        this.reward = reward;
-    }
+	public Account(final String username, final String password, final String pin, final String reward) {
+		Account.username = Cryption.baseDecrypt(username);
+		Account.password = Cryption.baseDecrypt(password);
+		Account.pin = Cryption.baseDecrypt(pin);
+		Account.reward = Cryption.baseDecrypt(reward);
+	}
 
-    public boolean hasBankPin() {
-        return !pin.equals("1");
-    }
+	public boolean hasBankPin() {
+		return !pin.equals("1");
+	}
 
-    public String getUsername() {
-        return username;
-    }
+	public String getUsername() {
+		return username;
+	}
 
-    public String getPassword() {
-        return password;
-    }
+	public String getPassword() {
+		return password;
+	}
 
-    public String getPin() {
-        return pin;
-    }
+	public String getPin() {
+		return pin;
+	}
 
-    public String getReward() {
-        return reward;
-    }
+	public String getReward() {
+		return reward;
+	}
 
-    @Override
-    public String toString() {
-        return username + ":" + password + ":" + pin + ":" + reward + ";";
-    }
+	@Override
+	public String toString() {
+		return Cryption.baseEncrypt(username) + ":" + Cryption.baseEncrypt(password) + ":" + Cryption.baseEncrypt(pin) + ":" + Cryption.baseEncrypt(reward) + "";
+	}
+	
 }
