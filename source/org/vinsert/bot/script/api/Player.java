8fe6a9959d270230b2d196ecfa8f685cce1db97a
Sequence-dk <sequence@hotmail.dk>
2013-05-13 23:30:08 +0300

    Update Player.java
    added isSpecialEnabled() and getSpecialEnergy()

--- source/org/vinsert/bot/script/api/Player.java
+++ source/org/vinsert/bot/script/api/Player.java
@@ -106,6 +106,23 @@ public class Player extends Actor {
     public int getNpcTrans() {
         return player.getComposite().getNpcTrans();
     }
+    
+    /**
+     * Returns if the special-attack option is enabled or not.
+	 *
+	 * @return true if special is enabled; else false.
+	 */
+	public boolean isSpecialEnabled() {
+        return ctx.settings.get(301) == 1;
+    }
 
+	/**
+	 * Gets the special bar energy in percentage.
+	 *
+     * @return The current special bar energy in percentage
+	 */
+	public int getSpecialEnergy() {
+        return (ctx.settings.get(300) / 10);
+    }
 
 }
